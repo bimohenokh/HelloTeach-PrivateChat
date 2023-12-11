2a8750a596eb029dc9d7e2205d31e9db60257767
bimo.henokh <bimo.henokh@ui.ac.id>
2023-12-11 23:59:03 +0700

    fixing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
-// import DaftarGuru from './component/DaftarGuru';
-// Basic samples
-import BasicGroupChannelSample from './samples/BasicGroupChannelSample';
-import CobaPrivateChat from "./samples/PrivateChat";
-import CobaPrivateChatNew from "./samples/CobaPrivateChatNew";
 import PrivateChat from "./samples/PrivateChat";
 
 const App = () => {
